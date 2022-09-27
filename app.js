207540dbfbaa9317ca9152f1753a71ceea77fa04
sinfon <sinfon@163.com>
2022-09-27 22:19:21 +0800

    add weui-wxss tpl && searchbar

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
-// app.js
-App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+require('./libs/Mixins.js');
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
+const listeners = [];
+
+App({
   globalData: {
-    userInfo: null
-  }
-})
+    theme: 'light', // dark
+    mode: '', // 模式(care：关怀模式)
+  },
+  changeGlobalData(data) {
+    this.globalData = Object.assign({}, this.globalData, data);
+    listeners.forEach((listener) => {
+      listener(this.globalData);
+    });
+  },
+  watchGlobalDataChanged(listener) {
+    if (listeners.indexOf(listener) < 0) {
+      listeners.push(listener);
+    }
+  },
+  unWatchGlobalDataChanged(listener) {
+    const index = listeners.indexOf(listener);
+    if (index > -1) {
+      listeners.splice(index, 1);
+    }
+  },
+  onThemeChange(resp) {
+    this.changeGlobalData({
+      theme: resp.theme,
+    });
+  },
+  onLaunch() {
+    // TODO: 检测适老化
+  },
+});
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,48 +1,79 @@
-// index.js
-// 获取应用实例
-const app = getApp()
-
 Page({
+  mixins: [require('../../mixin/common')],
   data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    canIUseGetUserProfile: false,
-    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName') // 如需尝试获取用户信息可改为false
+    inputShowed: false,
+    inputVal: '',
+    list: [
+      {
+        id: 'form',
+        name: '表单',
+        open: false,
+        pages: ['button', 'input', 'form', 'list', 'slideview', 'slider', 'uploader'],
+      },
+      {
+        id: 'layout',
+        name: '基础组件',
+        open: false,
+        pages: ['article', 'badge', 'flex', 'footer', 'gallery', 'grid', 'icons', 'loading', 'loadmore', 'panel', 'preview', 'progress', 'steps'],
+      },
+      {
+        id: 'feedback',
+        name: '操作反馈',
+        open: false,
+        pages: ['actionsheet', 'dialog', 'half-screen-dialog', 'msg', 'picker', 'toast', 'top-tips'],
+      },
+      {
+        id: 'nav',
+        name: '导航相关',
+        open: false,
+        pages: ['navigation-bar', 'tabbar'],
+      },
+      {
+        id: 'search',
+        name: '搜索相关',
+        open: false,
+        pages: ['searchbar'],
+      },
+    ],
   },
-  // 事件处理函数
-  bindViewTap() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad() {
-    if (wx.getUserProfile) {
-      this.setData({
-        canIUseGetUserProfile: true
-      })
-    }
-  },
-  getUserProfile(e) {
-    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
-    wx.getUserProfile({
-      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
-      success: (res) => {
-        console.log(res)
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+  kindToggle(e) {
+    const { id } = e.currentTarget; const { list } = this.data;
+    for (let i = 0, len = list.length; i < len; ++i) {
+      if (list[i].id == id) {
+        list[i].open = !list[i].open;
+      } else {
+        list[i].open = false;
       }
-    })
-  },
-  getUserInfo(e) {
-    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
-    console.log(e)
+    }
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  }
-})
+      list,
+    });
+  },
+  changeTheme() {
+    console.log(this.data);
+    getApp().themeChanged(this.data.theme === 'light' ? 'dark' : 'light');
+  },
+
+  // 搜索框
+  showInput() {
+    this.setData({
+      inputShowed: true,
+    });
+  },
+  hideInput() {
+    this.setData({
+      inputVal: '',
+      inputShowed: false,
+    });
+  },
+  clearInput() {
+    this.setData({
+      inputVal: '',
+    });
+  },
+  inputTyping(e) {
+    this.setData({
+      inputVal: e.detail.value,
+    });
+  },
+});
